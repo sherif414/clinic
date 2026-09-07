ef7717211edf32c9784f78ef3f4d06e2371c5689
shareef <70265506+sherif414@users.noreply.github.com>
2026-09-07 14:31:05 +0200

    Delete .wrangler directory

--- .wrangler/tmp/pages-1muxPD/chunks/build/index-styles.5dCJly5j.mjs
+++
@@ -1,2 +0,0 @@
-const t=["@media print{.no-print[data-v-81929041]{display:none!important}.appointment-confirmed-view[data-v-81929041]{padding:0!important}}"];export{t as default};
-//# sourceMappingURL=index-styles.5dCJly5j.mjs.map
